482f2165c5145687bab114034092ccead280bc75
Joao Mendes <j237881@dac.unicamp.br>
2024-10-01 18:13:48 -0300

    begin v2v code

---
+++ codes/Carro Seguidor/Core/Inc/v2v.h
@@ -0,0 +1,13 @@
+/*
+ * v2v.h
+ *
+ *  Created on: Oct 1, 2024
+ *      Author: jvmen
+ */
+
+#ifndef INC_V2V_H_
+#define INC_V2V_H_
+
+fsdanuifosdbnfbçdsufb
+
+#endif /* INC_V2V_H_ */
---
+++ codes/Carro Seguidor/Core/Src/v2v.c
@@ -0,0 +1,9 @@
+/*
+ * v2v.c
+ *
+ *  Created on: Oct 1, 2024
+ *      Author: jvmen
+ */
+
+
+jrgiosjfihçsdfoihsdfihasd
